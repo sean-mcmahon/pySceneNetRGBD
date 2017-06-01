90a1209aa8ad593758047853260b385accdf632c
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-06-01 16:56:05 +1000

    will not be reading from tar will extraqct into some dir then priodess

--- load_views.py
+++ load_views.py
@@ -18,6 +18,22 @@ except ImportError:
 from PIL import Image  # a part of caffe module on HPC
 
 
+def search_tar(members, pattern):
+    """
+    Loop over contents find the files with pattern in.
+    Return a generator to loop over the img files once.
+    """
+    print 'searching for', pattern
+    matchfound = False
+    for tarinfo in members:
+        if pattern in tarinfo.name:
+            matchfound = True
+            print 'match found'
+            return
+    if not matchfound:
+        raise(Exception('Could not find %s in tar file' % pattern))
+
+
 def tarobj_to_datum(tarobj, cv_flag=1):
     byte_arr = bytearray(tarobj.read())
     # cv_flag 1 for load color and cv_flag=2 for load depth
@@ -31,6 +47,23 @@ def tarobj_to_datum(tarobj, cv_flag=1):
         raise(Exception('cv_flag be 1 or 2!'))
     cv_img = cv2.imdecode(np.asarray(byte_arr, dtype=dtype_), cv_flag)
     im_ = cv_img[:, :, ::-1]  # bgr
+    im_ = im_.transpose((2, 0, 1))  # ch, h, w
+    datum = caffe.proto.caffe_pb2.Datum()
+    datum.channels = im_.shape[0]
+    datum.height = im_.shape[1]
+    datum.width = im_.shape[2]
+    datum.data = im_.tobytes()
+    return datum
+
+
+def get_datum(img_path, dtype_=None):
+    pil_img = Image.open(img_path)
+    # do data types have to be the same for caffe input data?
+    if dtype_:
+        im_ = np.array(pil_img, dtype=dtype_)
+    else:
+        im_ = np.array(pil_img)
+    im_ = im_[:, :, ::-1]  # bgr
     im_ = im_.transpose((2, 0, 1))  # ch, h, w
     datum = caffe.proto.caffe_pb2.Datum()
     datum.channels = im_.shape[0]
@@ -76,14 +109,14 @@ if __name__ == '__main__':
     num_traj = len(trajectories.trajectories)
     max_size = 2 * num_traj * img_per_traj * \
         (rgb_max + d_max + label_max)  # ~6GB (double for safety)
-    env_rgb = lmdb.open(dataset + '_lmdb', map_size=max_size)
+    env_rgb = lmdb.open('../' + dataset + '_lmdb', map_size=max_size)
 
     # nested for loop. Outer loop over random frame_ids inner loops over shuffles
     # trajectories iterable.
     r_frame_ids = random.sample(xrange(img_per_traj), img_per_traj)
     # random.shuffle(trajectories.trajectories)  # error AttError setitem
     rand_trajectories = random.sample(trajectories.trajectories, num_traj)
-    tar = tarfile.open(tarfilename, 'r')
+    tar = tarfile.open(tarfilename, 'r:gz')
     for f_count, frame_id in enumerate(r_frame_ids):
         txn = env_rgb.begin(write=True)
         print 'Saving frame_id {} into trajectories. {}/{}'.format(
@@ -94,6 +127,9 @@ if __name__ == '__main__':
             view = traj.views[frame_id]
             tar_img_name = str(os.path.join(traj.render_path, 'photo',
                                             str(view.frame_num)))
+            searchtime = time.time()
+            search_tar(tar, tar_img_name)
+            print 'search took ', time.time() - searchtime
             tartime = time.time()
             tarobj = tar.extractfile(tar_img_name)
             tartimes.apend(time.time() - tartime)
@@ -104,9 +140,10 @@ if __name__ == '__main__':
             txn.put(str_id.encode('ascii'), datum.SerializeToString())
             # if count % 10 == 0:
             print 'Processed {}/{} images'.format(count + 1, num_traj)
+            print 'Ext tme {}, datum time {}'.format(tartime[-1], dattimes[-1])
         txn.commit()
         print 'Avg extract time: {}\nAvg datum process time: {}'.format(
-            sum(tartimes) / len(tartimes), sum(dattimes)/len(dattimes))
+            sum(tartimes) / len(tartimes), sum(dattimes) / len(dattimes))
         print '-' * 50
     tar.close()
     env_rgb.close()
