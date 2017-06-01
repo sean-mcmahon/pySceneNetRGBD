de78606008ad1d2a9f0a7d54ce068f754661c749
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-06-01 14:10:59 +1000

    added in code to convert tar obj into array and reorder as per caffe and then convert back to bytes. Add other various updates; code almost ready

--- load_views.py
+++ load_views.py
@@ -7,6 +7,7 @@ import pdb
 import random
 import tarfile
 import io
+import cv2
 try:
     import caffe
 except ImportError:
@@ -16,6 +17,28 @@ except ImportError:
     subprocess.call(cmd, shell=True)
     import caffe
 from PIL import Image  # a part of caffe module on HPC
+
+
+def tarobj_to_datum(tarobj, cv_flag=1):
+    byte_arr = bytearray(tarobj.read())
+    # cv_flag 1 for load color and cv_flag=2 for load depth
+    if cv_flag == 1:
+        # colour image
+        dtype_ = np.uint8
+    elif cv_flag == 2:
+        # depth or instance image
+        dtype_ = np.uint16  # depth images are uin16s (cv takes 16 or 32 bit)
+    else:
+        raise(Exception('cv_flag be 1 or 2!'))
+    cv_img = cv2.imdecode(np.asarray(byte_arr, dtype=dtype_), cv_flag)
+    im_ = cv_img[:, :, ::-1]  # bgr
+    im_ = im_.transpose((2, 0, 1))  # ch, h, w
+    datum = caffe.proto.caffe_pb2.Datum()
+    datum.channels = im_.shape[0]
+    datum.height = im_.shape[1]
+    datum.width = im_.shape[2]
+    datum.data = im_.tobytes()
+    return datum
 
 
 def get_data_proto_paths(data_split='val'):
@@ -28,7 +51,6 @@ def get_data_proto_paths(data_split='val'):
 
 
 if __name__ == '__main__':
-    error_check = True
     trajectories = sn.Trajectories()
     # upper size of rgb ~37,908 bytes
     rgb_max = 37908
@@ -41,59 +63,47 @@ if __name__ == '__main__':
     #  adding cap to make sure nothing crazy is being written (1TB is fine tho)
     max_size = 2 * num_scenes * img_per_traj * \
         (rgb_max + d_max + label_max)  # ~6GB
-    env = lmdb.open('train_0', map_size=max_size)
+    env_rgb = lmdb.open('train_0', map_size=max_size)
 
-    dataset = 'train_0'
+    dataset = 'train_1'
     [data_root_path, protobuf_path] = get_data_proto_paths(dataset)
+    import pdb; pdb.set_trace()
+    tarfilename = '../{}.tar.gz'.format(dataset)
+    if not os.path.isfile(tarfilename):
+        raise(Exception('Invalid tarfilename: %s' % tarfilename))
     try:
         with open(protobuf_path, 'rb') as f:
             trajectories.ParseFromString(f.read())
     except IOError:
         print('Scenenet dataset not found at location:{0}'.format(
             data_root_path))
-        print('Protobuf file (.pb) not found at: {0}'.format(protobuf_path))
+        print('Or protobuf file (.pb) not found at: {0}'.format(protobuf_path))
         print('Please ensure you have copied the pb file to the data directory')
 
     # get random idx's into train set
     num_imgs = len(trajectories.trajectories) * \
         img_per_traj  # 300 imgs per traj
     print 'generating {} random (non repeating) indices'.format(num_imgs)
-    view_idx = random.sample(xrange(img_per_traj), img_per_traj)
+    # nested for loop. Outer loop over random frame_ids inner loops over shuffles
+    # trajectories iterable.
+    r_frame_ids = random.sample(xrange(img_per_traj), img_per_traj)
     random.shuffle(trajectories.trajectories)
     tar = tarfile.open(tarfilename, 'r')
-    for vi in view_idx:
-        txn = env.begin(write=True)
+    for frame_id in r_frame_ids:
+        txn = env_rgb.begin(write=True)
+        print 'saving fr'
         for traj in trajectories.trajectories:
-            view = traj.views[vi]
-            for img_type, n_c in zip(['photo', 'depth', 'instance'],
-                                     [3, 1, 1]):
-                tar_img_name = os.path.join(traj.render_path, img_type,
-                                            view.frame_num)
-                img = tar.getmember(tar_img_name)
-                byte_arr = bytearray(img.read())
-                if error_check:
-                    pil_img = Image.open(io.BytesIO(byte_arr))
-                    np_img = np.array(pil_img)
-                    if n_c != np_img.shape(2):
-                        print 'img shape: ', np.shape(np_img)
-                        raise(
-                            Exception(
-                                'expected num of channels does not match'))
-                datum = caffe.proto.caffe_pb2.Datum()
-                datum.channels = n_c
-                datum.height = 240
-                datum.width = 320
-                # TODO caffe takes weird shapes of arrays! Check if this
-                # works
-                datum.data = byte_arr
-                datum.encoded = True
-                str_id = tar_img_name.replace('/', '-')
-                txn.put(str_id.encode('ascii'), datum.SerializeToString())
-                print 'Breaking img_type loop Early!'
-                break
+            view = traj.views[frame_id]
+            tar_img_name = os.path.join(traj.render_path, 'photo',
+                                        view.frame_num)
+            tarobj = tar.extractfile(tar_img_name)
+            datum = tarobj_to_datum(tarobj, cv_flag=1)
+            str_id = tar_img_name.replace('/', '-')
+            txn.put(str_id.encode('ascii'), datum.SerializeToString())
             print 'Breaking traj loop Early!'
             break
-        txn.close()
+        txn.commit()
         print 'Breaking view loop Early!'
         break
     tar.close()
+    env_rgb.close()
