a572cca763f9ecab9097f9e56439e2c57b27e60d
Sean McMahon <s1.mcmahon@hdr.qut.edu.au>
2017-06-06 12:53:06 +1000

    code is ready to test, clean up, img encoding and saving all as float32s

--- load_views.py
+++ load_views.py
@@ -16,7 +16,7 @@ except ImportError:
     subprocess.call(cmd, shell=True)
     import caffe
 from PIL import Image  # a part of caffe module on HPC
-import scenenet_pb2 as sn  # a part of caffe module on HPC
+# import scenenet_pb2 as sn  # a part of caffe module on HPC
 
 
 def search_tar(members, pattern):
@@ -64,7 +64,8 @@ def tarobj_to_datum(tarobj, cv_flag=1, compress=True):
     cv_img = cv_img.astype(np.float32)
     datum = caffe.proto.caffe_pb2.Datum()
     if compress:
-        # Need to change image types (to float32), so cannot write compress tar img
+        # Need to change image types (to float32), so cannot write compress tar
+        # img
         datum.channels = cv_img.shape[2]
         datum.height = cv_img.shape[0]
         datum.width = cv_img.shape[1]
@@ -133,27 +134,37 @@ def loop_over_tar(tar_, lmdb_env, img_type, r_seed=378, early_stop=None):
         flag = 1
     else:
         flag = 2
-    rand_lim = 300 * 1000 * 3
     max_num = 1000 * 300
-    np.random.seed(r_seed)
     with lmdb_env.begin(write=True) as txn:
         start = time.time()
         for count, img in enumerate(img_gen):
             img_tarobj = tar_.extractfile(img)
             datum = tarobj_to_datum(img_tarobj, cv_flag=flag)
-            # str_id = '{}_{}'.format(
-            #     np.random.randint(0, rand_lim), img.name)
             str_id = img.name
-            print 'Image name: {}\nSize (bytes): {}'.format(img.name, sys.getsizeof(datum.SerializeToString()))
             txn.put(str_id.encode('ascii'), datum.SerializeToString())
             if count % 100 == 0:
                 tt = time.time() - start
-                print 'Saved {}/~{} images: Took {} s'.format(count, max_num, tt)
+                print 'Saved {}/~{} {} images: Took {} s'.format(count, max_num,
+                                                                 img_type, tt)
                 start = time.time()
             if early_stop and count >= early_stop:
                 print 'breaking early'
                 break
         print '{} images saved.'.format(count)
+
+
+def get_img_mem(tarfilename, img_type):
+    if img_type != 'photo' and img_type != 'depth' and img_type != 'instance':
+        raise(Exception('Unrecoginised img_type (%s), must be ' % img_type +
+                        '"photo", "depth" or "instance"'))
+    with tarfile.open(tarfilename, 'r|*') as tar_objs:
+        # open tar with stream (keeping things quick)
+        print 'Getting img filesize...'
+        for obj in tar_objs:
+            if img_type in obj.name and obj.isfile():
+                print 'Found %s img filesize' % img_type
+                return obj.size
+    raise(Exception('Coud not find file with "%s" in tarobj name' % img_type))
 
 
 if __name__ == '__main__':
@@ -164,7 +175,8 @@ if __name__ == '__main__':
     sup_dir = os.path.join(cyphy_dir, 'SeanMcMahon/datasets/SceneNet_RGBD')
     data_dir = os.path.join(cyphy_dir, 'SeanMcMahon/datasets/SceneNet_RGBD')
     # data_dir = '/tmp/n8307628'
-    trajectories = sn.Trajectories()
+    im_type = 'photo'
+    # trajectories = sn.Trajectories()
     # upper size of rgb ~37,908 bytes
     rgb_max = 37908
     # upper size of depth ~30,585 bytes
@@ -182,27 +194,15 @@ if __name__ == '__main__':
         raise(Exception('Could not find .pb file @ %s' % protobuf_path))
     if not os.path.isfile(tarfilename):
         raise(Exception('Could not find tar file @ %s' % tarfilename))
-    # try:
-    #     print 'Loading protobuf'
-    #     with open(protobuf_path, 'rb') as f:
-    #         trajectories.ParseFromString(f.read())
-    # except IOError:
-    #     print('Scenenet dataset not found at location:{0}'.format(
-    #         data_root_path))
-    #     print('Or protobuf file (.pb) not found at: {0}'.format(protobuf_path))
-    #     print('Please ensure you have copied the pb file to the data directory')
-    #     raise
-    num_traj = 1000  # len(trajectories.trajectories)
-    # TODO properly determine the required map size
-    max_size = 4 * num_traj * img_per_traj * \
-        (rgb_max + d_max + label_max)  # ~80GB (quad to make it fit)
-    env_rgb = lmdb.open(os.path.join(data_dir, dataset + '_test'),
-                        map_size=max_size)
-    tar = tarfile.open(tarfilename, 'r')
 
-    overall_time = time.time()
-    print 'looping over tar'
-    loop_over_tar(tar, env_rgb, 'photo', early_stop=1000)
-    print 'saving took {}'.format(time.time() - overall_time)
-    env_rgb.close()
-    tar.close()
+    num_traj = 1000  # len(trajectories.trajectories)
+    img_mem_ = get_img_mem(tarfilename, im_type)
+    # sometimes save imgs as float32, 4 times more memory than uint8 RGB pixels)
+    # and double for breathing room
+    max_size = 2 * num_traj * img_per_traj * 4 * img_mem_
+    lmdb_path = os.path.join(data_dir, dataset + '_' + im_type + '_test')
+    with lmdb.open(lmdb_path, map_size=max_size) as lmbd_env, tarfile.open(tarfilename, 'r') as tar:
+        overall_time = time.time()
+        print 'looping over tar'
+        loop_over_tar(tar, lmbd_env, im_type, early_stop=1000)
+        print 'saving took {}'.format(time.time() - overall_time)
